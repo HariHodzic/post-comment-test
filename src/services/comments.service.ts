929d5bb191e88ef97481017ec692c6b67d9c8e35
hari.hodzic98@gmail.com <hari.hodzic98@gmail.com>
2023-09-13 14:41:10 +0200

    more changes

--- src/services/comments.service.ts
+++ src/services/comments.service.ts
@@ -68,6 +68,7 @@ export class CommentsService {
   async getCommentByIdAndDoStuff(id: string): Promise<Comment> {
     const result = await this.commentsRepository.findOne({ where: { id: id } });
     result.content = result.content + 'stuff';
+    result.post = null;
     if (!result) {
       throw new NotFoundException(`Comment with ID "${id}" not found`);
     }
